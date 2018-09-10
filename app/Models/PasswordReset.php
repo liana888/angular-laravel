d0cf1a28f93ff75bdb4611670a89eb3472b88d92
liana888 <39312522+liana888@users.noreply.github.com>
2018-09-10 20:23:51 +0400

    Update PasswordReset.php

--- app/Models/PasswordReset.php
+++ app/Models/PasswordReset.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PasswordReset extends Model
 {
-    public $timestamps = false;
+    public $timestamps = true;
     public $incrementing = false;
 
     protected $primaryKey = 'email';
